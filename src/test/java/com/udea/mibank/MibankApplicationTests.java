30983364b6ea7764730550932b388a6c23cffdc0
Jeferson Alexander Del Rio Herrera <yeferdrh@gmail.com>
2025-04-10 21:00:20 -0500

    Initial project setup

---
+++ src/test/java/com/udea/mibank/MibankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.mibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
